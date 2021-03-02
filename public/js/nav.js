9fb9b634c41992bb33777e5c43cec262ecfdf5af
Tom <tom.soetinck@laposte.net>
2021-03-02 12:00:18 +0100

    more html

--- public/js/nav.js
+++ public/js/nav.js
@@ -8,8 +8,9 @@ bool = true
 function showNav() {
 
     if (bool) {
-        ul.style.transform = "translateX(0%)"
+        ul.style.transform = "translateX(0%) translateY(-20%)"
         circle.style.animationPlayState = "paused";
+        circle.style.opacity = "50%"
         svgCircle.style.animationPlayState = "paused";
         for (var i = 0, max = li.length; i < max; i++) {
             li[i].style.transform = "scaleX(100%)"
@@ -17,8 +18,9 @@ function showNav() {
         bool = false
     } else {
 
-        ul.style.transform = "translateX(-100%)"
+        ul.style.transform = "translateX(-100%)  translateY(-20%)"
         circle.style.animationPlayState = "running";
+        circle.style.opacity = "100%"
         svgCircle.style.animationPlayState = "running";
         for (var i = 0, max = li.length; i < max; i++) {
             li[i].style.transform = "scaleX(0%)"
