ff2af4d7e4c7407f3fea24c641977e21ae8bf3b8
Tom <tom.soetinck@laposte.net>
2021-03-03 20:46:16 +0100

    css

--- public/js/nav.js
+++ public/js/nav.js
@@ -13,19 +13,15 @@ function showNav() {
         circle.style.animationPlayState = "paused";
         circle.style.opacity = "50%"
         svgCircle.style.animationPlayState = "paused";
-        for (var i = 0, max = li.length; i < max; i++) {
-            li[i].style.transform = "scaleX(100%)"
-        }
+
         bool = false
     } else {
 
-        ul.style.transform = "translateX(-100%)  translateY(-20%)"
+        ul.style.transform = "translateX(-150%)  translateY(-20%)"
         circle.style.animationPlayState = "running";
         circle.style.opacity = "100%"
         svgCircle.style.animationPlayState = "running";
-        for (var i = 0, max = li.length; i < max; i++) {
-            li[i].style.transform = "scaleX(0%)"
-        }
+
         bool = true
 
     }
