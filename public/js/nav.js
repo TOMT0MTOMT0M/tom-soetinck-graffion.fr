ea76308e40a23ef0eee99fe727f1b88ac921aa38
Tom <tom.soetinck@laposte.net>
2021-03-04 19:26:43 +0100

    css footer nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,10 @@
 let circle = document.querySelector("#circle");
 let svgCircle = document.querySelector('#circle svg');
 let ul = document.querySelector("ul");
-let li = document.querySelectorAll("li");
+let li1 = document.querySelector(".li1");
+let li2 = document.querySelector(".li2");
+let li3 = document.querySelector(".li3");
+let li4 = document.querySelector(".li4");
 let hamburger = document.querySelector(".hamburgerT")
 
 bool = true
@@ -9,7 +12,10 @@ bool = true
 function showNav() {
     hamburger.classList.toggle("change");
     if (bool) {
-        ul.style.transform = "translateX(0%) translateY(65%)"
+        li1.style.transform = "translateX(0%)"
+        li2.style.transform = "translateX(0%)"
+        li3.style.transform = "translateX(0%)"
+        li4.style.transform = "translateX(0%)"
         circle.style.animationPlayState = "paused";
         circle.style.opacity = "50%"
         svgCircle.style.animationPlayState = "paused";
@@ -17,7 +23,10 @@ function showNav() {
         bool = false
     } else {
 
-        ul.style.transform = "translateX(-150%)  translateY(65%)"
+        li1.style.transform = "translateX(-100%)"
+        li2.style.transform = "translateX(-100%)"
+        li3.style.transform = "translateX(-100%)"
+        li4.style.transform = "translateX(-150%)"
         circle.style.animationPlayState = "running";
         circle.style.opacity = "100%"
         svgCircle.style.animationPlayState = "running";
