fe231c6e39e570bda0dbf9e72d4009ac4b2ac4d1
Tom <tom.soetinck@laposte.net>
2021-03-05 23:36:36 +0100

    css

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,9 +1,5 @@
 const loader = document.querySelector('.loader');
-window.addEventListener('load', () => {
-    if (sessionStorage.getItem('premierChargement') === null) {
-        loader.classList.add('fondu-out');
-    } else {
-        loader.style.display = "none";
-    }
-    sessionStorage.setItem('premierChargement', 'done');
-})
+
+window.addEventListener('load', function() {
+    loader.style.display = "none";
+});
